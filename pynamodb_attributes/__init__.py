090ccf987a14d8663b4b79e5ecc59bc9ed7aa57d
Nolan Lum <nlum@lyft.com>
2019-06-12 13:55:50 -0700

    fix module import (0.2.2)

--- pynamodb_attributes/__init__.py
+++ pynamodb_attributes/__init__.py
@@ -1,11 +1,13 @@
 from .integer import IntegerAttribute
 from .integer_date import IntegerDateAttribute
+from .integer_enum import IntegerEnumAttribute
 from .unicode_delimited_tuple import UnicodeDelimitedTupleAttribute
 from .unicode_enum import UnicodeEnumAttribute
 
 __all__ = [
     'IntegerAttribute',
     'IntegerDateAttribute',
+    'IntegerEnumAttribute',
     'UnicodeDelimitedTupleAttribute',
     'UnicodeEnumAttribute',
 ]
