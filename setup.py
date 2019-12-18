48bc11c700ac4a84152bbd555b0f942dc9fdab04
th0114nd <1399455+th0114nd@users.noreply.github.com>
2019-12-18 13:04:35 -0500

    Belated version bump for https://github.com/lyft/pynamodb-attributes/pull/14

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def find_stubs(package):  # type: ignore
 
 setup(
     name='pynamodb-attributes',
-    version='0.2.5',
+    version='0.2.6',
     description='Common attributes for PynamoDB',
     url='https://www.github.com/lyft/pynamodb-attributes',
     maintainer='Lyft',
