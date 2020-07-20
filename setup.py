5368be1034741d93e69120dde2ebfa566d931e8c
Max Rozentsveyg <mxr@lyft.com>
2020-07-20 18:17:03 -0400

    Remove find_stubs

--- setup.py
+++ setup.py
@@ -1,18 +1,5 @@
-import os
-
 from setuptools import find_packages
 from setuptools import setup
-
-
-def find_stubs(package):  # type: ignore
-    stubs = []
-    for root, _, files in os.walk(package):
-        for f in files:
-            path = os.path.join(root, f).replace(package + os.sep, '', 1)
-            if path.endswith('.pyi') or path.endswith('py.typed'):
-                stubs.append(path)
-    return {package: stubs}
-
 
 setup(
     name='pynamodb-attributes',
@@ -27,5 +14,5 @@ setup(
         "pynamodb>=3.3.3",
     ],
     python_requires='>=3',
-    package_data=find_stubs('pynamodb_attributes'),
+    package_data={'pynamodb_attributes': ['py.typed']},
 )
