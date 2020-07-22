52a1c12ea5f7b1c2b079dc2be409f89b1f698e81
Max R <mxr@users.noreply.github.com>
2020-07-22 09:49:55 -0400

    Create TimedeltaAttribute (#22)

---
+++ pynamodb_attributes/timedelta.py
@@ -0,0 +1,44 @@
+from datetime import timedelta
+from typing import Any
+from typing import Optional
+
+import pynamodb
+
+from ._typing import Attribute
+
+
+class TimedeltaAttribute(Attribute[timedelta]):
+    """"
+    Stores a timedelta as a number of seconds (truncated).
+
+    >>> class MyModel(Model):
+    >>>   delta = TimedeltaAttribute(default=lambda: timedelta(seconds=5))
+    >>>   delta_ms = TimedeltaMsAttribute(default=lambda: timedelta(milliseconds=500))
+    """
+    attr_type = pynamodb.constants.NUMBER
+    _multiplier = 1.0
+
+    def deserialize(self, value: str) -> timedelta:
+        return timedelta(microseconds=float(value) * (1000000.0 / self._multiplier))
+
+    def serialize(self, td: timedelta) -> int:
+        return int(td.total_seconds() * self._multiplier)
+
+    def __set__(self, instance: Any, value: Optional[Any]) -> None:
+        if value is not None and not isinstance(value, timedelta):
+            raise TypeError(f"value has invalid type '{type(value)}'; Optional[timedelta] expected")
+        return super().__set__(instance, value)
+
+
+class TimedeltaMsAttribute(TimedeltaAttribute):
+    """"
+    Stores a timedelta as a number of milliseconds AKA ms (truncated).
+    """
+    _multiplier = 1000.0
+
+
+class TimedeltaUsAttribute(TimedeltaAttribute):
+    """"
+    Stores a timedelta as a number of microseconds AKA μs (truncated).
+    """
+    _multiplier = 1000000.0
---
+++ tests/timedelta_attribute_test.py
@@ -0,0 +1,71 @@
+from datetime import timedelta
+
+import pytest
+from pynamodb.attributes import UnicodeAttribute
+from pynamodb.models import Model
+
+from pynamodb_attributes import TimedeltaAttribute
+from pynamodb_attributes import TimedeltaMsAttribute
+from pynamodb_attributes import TimedeltaUsAttribute
+from tests.connection import _connection
+from tests.meta import dynamodb_table_meta
+
+
+class MyModel(Model):
+    Meta = dynamodb_table_meta(__name__)
+
+    key = UnicodeAttribute(hash_key=True)
+    value = TimedeltaAttribute(null=True)
+    value_ms = TimedeltaMsAttribute(null=True)
+    value_us = TimedeltaUsAttribute(null=True)
+
+
+@pytest.fixture(scope='module', autouse=True)
+def create_table():
+    MyModel.create_table()
+
+
+def test_serialization_non_null(uuid_key):
+    model = MyModel()
+    model.key = uuid_key
+    model.value = model.value_ms = model.value_us = timedelta(seconds=456, microseconds=123456)
+    model.save()
+
+    # verify underlying storage
+    item = _connection(MyModel).get_item(uuid_key)
+    assert item['Item']['value'] == {'N': '456'}
+    assert item['Item']['value_ms'] == {'N': '456123'}
+    assert item['Item']['value_us'] == {'N': '456123456'}
+
+    # verify deserialization
+    model = MyModel.get(uuid_key)
+    assert model.value == timedelta(seconds=456)
+    assert model.value_ms == timedelta(seconds=456, microseconds=123000)
+    assert model.value_us == timedelta(seconds=456, microseconds=123456)
+
+
+def test_serialization_null(uuid_key):
+    model = MyModel()
+    model.key = uuid_key
+    model.value = None
+    model.value_ms = None
+    model.value_us = None
+    model.save()
+
+    # verify underlying storage
+    item = _connection(MyModel).get_item(uuid_key)
+    assert 'value' not in item['Item']
+    assert 'value_ms' not in item['Item']
+    assert 'value_us' not in item['Item']
+
+    # verify deserialization
+    model = MyModel.get(uuid_key)
+    assert model.value is None
+    assert model.value_ms is None
+    assert model.value_us is None
+
+
+def test_set_invalid_type():
+    model = MyModel()
+    with pytest.raises(TypeError, match='invalid type'):
+        model.value = 42
