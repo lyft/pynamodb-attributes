80cdcaabb2b041329f520509142a0c13f33cba9d
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2022-08-26 14:03:48 -0400

    Move metadata from setup.py setup.cfg (#45)

--- setup.py
+++ setup.py
@@ -1,18 +1,3 @@
-from setuptools import find_packages
 from setuptools import setup
 
-setup(
-    name="pynamodb-attributes",
-    version="0.4.0",
-    description="Common attributes for PynamoDB",
-    url="https://www.github.com/lyft/pynamodb-attributes",
-    maintainer="Lyft",
-    maintainer_email="ikonstantinov@lyft.com",
-    packages=find_packages(exclude=["tests*"]),
-    dependency_links=[],
-    install_requires=[
-        "pynamodb>=5.0.0",
-    ],
-    python_requires=">=3",
-    package_data={"pynamodb_attributes": ["py.typed"]},
-)
+setup()
